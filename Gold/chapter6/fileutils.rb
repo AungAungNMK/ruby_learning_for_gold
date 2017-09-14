2195db6890b6b31f622ca58c926399df470a2a66
aung aung <arkarwunna890.zz@gmail.com>
2017-09-14 11:37:07 +0630

    fileutilies

---
+++ Gold/chapter6/fileutils.rb
@@ -0,0 +1,30 @@
+#cdpwd
+FileUtils.cd(dir, options = {})
+FileUtils.pwd
+# cp/copy
+FileUtils.cd(src, desc, options ={})
+FileUtils.copy(src, desc, options = {})
+#cp_r
+FileUtils.cp_r(src, desc, options = {})
+#mv / move
+FileUtils.mv(src, desc, options = {})
+FileUtils.move(src, desc, options = {})
+#rm/remove
+FileUtils.rm(list, options = {})
+FileUtils.remove(list, options = {})
+#rm_dir
+FileUtils.rmdir(list, options = {})
+#rm_r /rm_f
+FileUtils.rm_r(list,options = {})
+FileUtils.rm_rf(list, options = {})
+#touch
+FileUtils.touch(list, options = {})
+#mkdir / mkfir_p
+FileUtils.mkdir(list, options = {})
+FileUtils.mkfir_p(list, options = {})
+#chown / chmod
+FileUtils.chown(user, group, list, options = {})
+FileUtils.chmod(list, options = {})
+#in_s / symlink
+FileUtils.ln_s(src, desc, options = {})
+FileUtils.symlink(src, desc, options = {})
